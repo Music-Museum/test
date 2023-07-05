1bb6d273ef4025285a4d69c1f6632de00d95aa7c
Music-Museum <770738225@qq.com>
2023-07-05 13:11:15 +0800

    Add files via upload

---
+++ Otest.py
@@ -0,0 +1,17 @@
+# coding:utf-8  头部注释，告诉系统脚本编码，更好的解释中文。
+# 注释，井号后加空格
+# import os
+# print(os.getcwd()) #获取当前路径
+
+"""
+
+三引号也是注释，用来注释比较长的内容
+"""
+# 加号连接是没有空格的
+info1='123'
+info2='345'
+print(info1+info2+'888')
+
+# 布尔可以用在判断数字类型，和字符串，str'' 是否空字符串，经常也会用10来替代true和false.''和None也代表false
+# a=1
+# print(bool(a)),true,bool布尔.
