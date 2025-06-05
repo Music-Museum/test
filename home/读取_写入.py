119eaec67ff95ee0b1977fbd385006cda986958f
Music-Museum <770738225@qq.com>
2025-06-06 00:08:59 +0800

    Delete home/读取_写入.py

--- home/读取_写入.py
+++
@@ -1,9 +0,0 @@
-# coding:UTF-8
-''''
-import pandas as pd
-data = pd.read_excel('1.xlsx', sheet_name='夹板')  # 可以将括号内文件相对路径（脚本所在路径）设置成绝对路径
-
-import pandas as pd
-data = pd.DataFrame([[1, 2], [3, 4]], columns=['A', 'B'])
-data.to_excel(r'C:\Users\Administrator\Desktop\1.xlsx',index=False)  # 将DataFrame中的数据写入工作簿,忽略行标题
-'''
